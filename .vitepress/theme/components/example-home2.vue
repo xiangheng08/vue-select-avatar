a31de1b127657acb4fc1066018a03842601c6562
laowan <11513313+laowans@user.noreply.gitee.com>
2025-05-31 00:48:33 +0800

    feat(example-home2): 添加重新选择按钮
    - 在 Viewport 组件下方添加重新选择按钮
    - 按钮点击时触发 viewportRef 的 select 方法
    - 调整样式以确保按钮居中并设置合适的间距

--- .vitepress/theme/components/example-home2.vue
+++ .vitepress/theme/components/example-home2.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
-import { Viewport, Preview } from '../../../lib'
 import { loadCatImage } from '../utils/image'
+import { Viewport, Preview } from '../../../lib'
 
 const viewportRef = ref<InstanceType<typeof Viewport>>()
 
@@ -14,10 +14,42 @@ onMounted(async () => {
 <template>
   <div style="display: flex; flex-direction: column; align-items: center; margin-top: 50px">
     <div style="display: flex; gap: 40px; align-items: center">
-      <Viewport ref="viewportRef" :size="180" fixed-image />
+      <div style="position: relative">
+        <Viewport ref="viewportRef" :size="180" fixed-image />
+        <button
+          style="
+            position: absolute;
+            left: 50%;
+            top: calc(100% + 4px);
+            transform: translateX(-50%);
+            font-size: 12px;
+            display: flex;
+            align-items: center;
+          "
+          @click="viewportRef?.select()"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1em"
+            height="1em"
+            viewBox="0 0 16 16"
+            style="display: inline; margin-right: 4px"
+          >
+            <path
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="1.5"
+              d="M4.75 10.75h-3m12.5-2c0 3-2.798 5.5-6.25 5.5c-3.75 0-6.25-3.5-6.25-3.5v3.5m9.5-9h3m-12.5 2c0-3 2.798-5.5 6.25-5.5c3.75 0 6.25 3.5 6.25 3.5v-3.5"
+            />
+          </svg>
+          重新选择
+        </button>
+      </div>
       <div style="width: 1px; height: 180px; background-color: var(--vp-c-border)"></div>
       <Preview :viewport-ref="viewportRef" :size="96" round />
     </div>
-    <div style="margin-top: 20px; font-size: 13px">仿哔哩哔哩头像选择</div>
+    <div style="margin-top: 50px; font-size: 13px">仿哔哩哔哩头像选择</div>
   </div>
 </template>
